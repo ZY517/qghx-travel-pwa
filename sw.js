e8dda5926aa527ccc15d074dfc8cbb49d4fd8d24
QGHX <qghx@travel.app>
2026-07-22 16:55:04 +0800

    青甘大环线PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'qghx-v2'
+const ASSETS = [
+  '/', '/index.html', '/manifest.json',
+  '/icons/icon-192.png', '/icons/icon-512.png'
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)))
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+      }
+      return res
+    }))
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ))
+})
